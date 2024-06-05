c7395c76a373017e07da82482430e8fe29284436
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2024-06-05 19:49:42 -0400

    separando back e front

--- install_dotnet.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Baixe e instale o .NET SDK
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh --install-dir ~/.dotnet --version 8.0  # ou a versão que você precisa
-export PATH="$HOME/.dotnet:$HOME/.dotnet/tools:$PATH"
