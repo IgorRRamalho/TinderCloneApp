038b5783fd388e5381ed66b0f75f074385391edd
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2024-06-05 20:26:20 -0400

    back separado

---
+++ server/Models/User.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+using System.Collections.Generic;
+
+namespace TinderClone.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; } = null!;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+
+        [BsonElement("gender")]
+        public string Gender { get; set; } = null!;
+
+        [BsonElement("bio")]
+        public string Bio { get; set; } = null!;
+
+        [BsonElement("photos")]
+        public List<string> Photos { get; set; } = null!;
+
+        [BsonElement("location")]
+        public GeoJsonPoint<GeoJson2DCoordinates> Location { get; set; } = null!;
+
+        [BsonElement("preferences")]
+        public Preferences Preferences { get; set; } = null!;
+
+        [BsonElement("interests")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string> Interests { get; set; } = null!;
+    }
+
+    public class Preferences
+    {
+        [BsonElement("gender")]
+        public string Gender { get; set; } = null!;
+
+        [BsonElement("ageRange")]
+        public List<int> AgeRange { get; set; } = null!;
+
+        [BsonElement("distance")]
+        public int Distance { get; set; }
+    }
+}
